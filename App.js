29b80bd3fc9441d335fcb58a5c822fcadf4e098f
dianKoliva <dianpretty01@gmail.com>
2023-02-21 16:27:58 +0200

    new changes

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
-import HomeStack from './src/routes/HomeStack';
-import Destination from './src/components/Destination';
-import Available from './src/components/Available';
-import Ticket from './src/components/Ticket';
-import Seats from './src/components/Seats';
 import NavigationStack from './src/routes/NavigationStack';
-import Payment from './src/components/Payment';
 import { Provider } from 'react-redux';
 // import { store } from './src/redux/store';
 
